937e3be91eb8c88f2fa7c11c57a3e9dc71c04fdb
alexrussoeire <alexrussoeire@gmail.com>
2017-12-30 00:20:12 +0000

    Added the MQTT user configuration template.
    Fields need to be updated before programming the board.

---
+++ controller/UserConfig.h
@@ -0,0 +1,7 @@
+const char* ssid = "WIFI SSID";
+const char* password = "WIFI PASSWORD";
+const char* mqtt_server = "MQTT SERVER IP ADDRESS";
+
+//Unique device ID
+const char* mqttDeviceID = "MQTT DEVICE NAME";
+
